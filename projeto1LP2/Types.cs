9d9c837461c89ecbd7322deed27d492709143dbd
FranciscoPires <frelvaspires@gmail.com>
2020-11-26 13:57:41 +0000

    comentarios adicionados, ficheiro csv removido

--- projeto1LP2/Types.cs
+++ projeto1LP2/Types.cs
@@ -14,29 +14,30 @@ namespace projeto1LP2
         public int Disc_Year { get; set; }
         // Período orbital (em dias).
         public float Pl_Orbper { get; set; } 
-        // Raio do planeta (em comparação com a Terra)
+        // Raio do planeta (em comparação com a Terra).
         public float Pl_Rade { get; set; } 
-         // Massa do planeta (em comparação com a Terra)
+         // Massa do planeta (em comparação com a Terra).
         public float Pl_Masse { get; set; } 
         // Temperatura do equilíbrio do planeta (em Kelvins).
         public int Pl_Eqt { get; set; } 
         // Temperatura efetiva da estrela (em Kelvins).
         public float St_Teff { get; set; } 
-        // Raio da estrela (em comparação com o Sol
+        // Raio da estrela (em comparação com o Sol).
         public float St_Rad { get; set; } 
-        // Massa da estrela (em comparação com o Sol
+        // Massa da estrela (em comparação com o Sol).
         public float St_Mass { get; set; }
         /* Idade da estrela (em Giga-anos, ou seja, por milhar de 
          * milhão de anos).*/
         public float St_Age { get; set; } //não tem
-        // Velocidade de rotação da estrela (em km/s)
+        // Velocidade de rotação da estrela (em km/s).
         public float St_Vsin { get; set; } //não tem
-        // Período de rotação da estrela (em dias)
-        public float St_Rotp { get; set; } //não tem
+        // Período de rotação da estrela (em dias).
+        public float St_Rotp { get; set; } //não tem.
         /* Distância entre a Terra e a estrela (em Parsecs, 
          * sendo 1 Parsec ≈ 3,26 anos-luz).*/
         public float Sy_Dist { get; set; } 
 
+        // Construtor onde são inicializadas as variáveis.
         public Types(string pl_name, string hostname, string discoverymethod,
             int disc_year, float pl_orbper, float pl_rade, float pl_masse, 
             int pl_eqt, float st_teff, float st_rad, float st_mass, 
