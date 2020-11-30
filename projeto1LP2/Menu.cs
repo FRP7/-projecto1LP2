a577788ac6b50c714ac27874dd5ee78bae61a0c9
Nuno <nunosilvares@hotmail.com>
2020-11-30 16:15:35 +0000

    Changes to the menu

--- projeto1LP2/Menu.cs
+++ projeto1LP2/Menu.cs
@@ -12,16 +12,62 @@ namespace projeto1LP2
         public void MainMenu() {
             Facade facade = new Facade();
             facade.ReadFile();
-            // Mostrar planetas (id e nome, só para testar)
-            /*foreach (KeyValuePair<int, Planet> item in Facade.planetList) {
-                Console.WriteLine($"ID: {item.Key} | Planeta: {item.Value.Pl_Name}");
-            }*/
-
-            // Mostrar estrelas (id, nome e nº de planetasm só para testar)
-            /*foreach (KeyValuePair<int, Star> item in Facade.starList) {
-                Console.WriteLine($"ID: {item.Key} | Estrela: {item.Value.HostName}" +
-                 $" | Planetas: {item.Value.St_PlCount}");
-            }*/
+            Console.WriteLine("\nWelcome to the Interstelar sorter.\n\nWhat would you like to sort?\n\n\t1 = Planets\n\t2 = Stars\n\tEsc = Exit");
+            bool retry;
+            do
+            {
+                ConsoleKeyInfo choice = Console.ReadKey();
+                retry = false;
+                switch (choice.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.Write("\nYou have chosen Planets, standby for results");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.Write(" .");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.Write(" .");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.WriteLine(" .\n");
+                        System.Threading.Thread.Sleep(1000);
+                        // Mostrar planetas (id e nome, só para testar)
+                        foreach (KeyValuePair<int, Planet> item in Facade.planetList)
+                        {
+                            Console.WriteLine(string.Format($"ID: {item.Key,-5} | Planeta: {item.Value.Pl_Name,-30}"));
+                        }
+                        break;
+                    case ConsoleKey.D2:
+                        Console.Write("\nYou have chosen Stars, standby for results");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.Write(" .");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.Write(" .");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.WriteLine(" .\n");
+                        System.Threading.Thread.Sleep(1000);
+                        // Mostrar estrelas (id, nome e nº de planetas só para testar)
+                        foreach (KeyValuePair<int, Star> item in Facade.starList)
+                        {
+                            Console.WriteLine(string.Format($"ID: {item.Key,-5} | Estrela: {item.Value.HostName,-30}" +
+                             $" | Planetas: {item.Value.St_PlCount,-2}"));
+                        }
+                        break;
+                    case ConsoleKey.Escape:
+                        Console.Write("\n_Goodbye");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.Write(" .");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.Write(" .");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.WriteLine(" .\n");
+                        System.Threading.Thread.Sleep(1000);
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("\nUnknown Command, please try again.");
+                        retry = true;
+                        break;
+                }
+            } while (retry == true);
         }
     }
 }
