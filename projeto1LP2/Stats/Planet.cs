9850d1c4ba6d5702594a551186199f59e61825ce
FranciscoPires <frelvaspires@gmail.com>
2020-12-05 19:21:58 +0000

    Código do Planet devidamente comentado.

--- projeto1LP2/Stats/Planet.cs
+++ projeto1LP2/Stats/Planet.cs
@@ -49,14 +49,43 @@ namespace projeto1LP2
 
         /* Distância entre a Terra e a estrela (em Parsecs, 
          * sendo 1 Parsec ≈ 3,26 anos-luz).*/
-        public Nullable<double> Sy_Dist { get; set; } 
+        public Nullable<double> Sy_Dist { get; set; }
 
-        // Inicializar as propriedades.
+        /// <summary>
+        /// Inicializar as propriedades.
+        /// </summary>
+        /// <param name="pl_Name"> Nome do planeta. </param>
+        /// <param name="hostName"> Nome da estrela que o planeta orbita. 
+        /// </param>
+        /// <param name="discoveryMethod"> Método de descoberta. </param>
+        /// <param name="disc_Year"> Ano de descoberta. </param>
+        /// <param name="pl_Orbper"> Período orbital (em dias). </param>
+        /// <param name="pl_Rade"> Raio do planeta (em comparação com a Terra). 
+        /// </param>
+        /// <param name="pl_Masse"> Massa do planeta (em comparação
+        /// com a Terra. </param>
+        /// <param name="pl_Eqt"> Temperatura do equilíbrio do planeta
+        /// (em Kelvins). </param>
+        /// <param name="st_Teff"> Temperatura efetiva da estrela (em Kelvins).
+        /// </param>
+        /// <param name="st_Rad"> Raio da estrela (em comparação com o Sol). 
+        /// </param>
+        /// <param name="st_Mass"> Massa da estrela (em comparação com o Sol). 
+        /// </param>
+        /// <param name="st_Age"> Idade da estrela (em Giga-anos, ou seja, 
+        /// por milhar de milhão de anos).</param>
+        /// <param name="st_Rotation"> Velocidade de rotação da estrela
+        /// (em km/s). </param>
+        /// <param name="st_Rotp"> Período de rotação da estrela (em dias). 
+        /// </param>
+        /// <param name="sy_Dist"> Distância entre a Terra e a estrela 
+        /// (em Parsecs, sendo 1 Parsec ≈ 3,26 anos-luz). </param>
         public Planet(string pl_Name, string hostName, string discoveryMethod,
             int? disc_Year, double? pl_Orbper, double? pl_Rade, double? pl_Masse,
             int? pl_Eqt, double? st_Teff, double? st_Rad, double? st_Mass,
             double? st_Age, double? st_Rotation, double? st_Rotp,
             double? sy_Dist) {
+
             Pl_Name = pl_Name;
             HostName = hostName;
             DiscoveryMethod = discoveryMethod;
