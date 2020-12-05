cd722f5f4a33044bf34b5a1377bee88609865b69
FranciscoPires <frelvaspires@gmail.com>
2020-12-05 19:24:55 +0000

    Código da Star devidamente comentado.

--- projeto1LP2/Stats/Star.cs
+++ projeto1LP2/Stats/Star.cs
@@ -36,13 +36,34 @@ namespace projeto1LP2
          * sendo 1 Parsec ≈ 3,26 anos-luz).*/
         public Nullable<double> Sy_Dist { get; set; }
 
-        // Número de planetas lidos que orbitam 
+        // Número de planetas lidos que orbitam.
         public Nullable<int> St_PlCount { get; set; }
 
-        // Inicializar as propriedades.
+
+        /// <summary>
+        /// Inicializar as propriedades.
+        /// </summary>
+        /// <param name="hostName"> Nome da estrela. </param>
+        /// <param name="discoveryMethod"> Método de descoberta. </param>
+        /// <param name="disc_Year"> Ano de descoberta. </param>
+        /// <param name="st_Teff"> Temperatura efetiva da estrela (em Kelvins). 
+        /// </param>
+        /// <param name="st_Rad"> Raio da estrela (em comparação com o Sol). 
+        /// </param>
+        /// <param name="st_Mass"> Massa da estrela (em comparação com o Sol). 
+        /// </param>
+        /// <param name="st_Age"> Idade da estrela (em Giga-anos, ou seja, 
+        /// por milhar de milhão de anos). </param>
+        /// <param name="st_Vsin"> Velocidade de rotação da estrela (em km/s). </param>
+        /// <param name="st_Rotp"> Período de rotação da estrela (em dias). </param>
+        /// <param name="sy_Dist"> Distância entre a Terra e a estrela 
+        /// (em Parsecs, sendo 1 Parsec ≈ 3,26 anos-luz). </param>
+        /// <param name="st_PlCount"> Número de planetas lidos que orbitam. 
+        /// </param>
         public Star(string hostName, string discoveryMethod, int? disc_Year, 
             double? st_Teff, double? st_Rad, double? st_Mass, double? st_Age, 
             double? st_Vsin, double? st_Rotp, double? sy_Dist, int? st_PlCount) {
+
             HostName = hostName;
             DiscoveryMethod = discoveryMethod;
             Disc_Year = disc_Year;
